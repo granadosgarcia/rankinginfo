cdf8ed2eab508e4aa9bbe0d084ac3b0f59a25ace
abrgnz <crazymarines2@gmail.com>
2012-12-09 13:08:32 -0600

    cambios a word.php

--- inicio/arrendado/consulta/descarga/word.php
+++ inicio/arrendado/consulta/descarga/word.php
@@ -1,7 +1,61 @@
 <?php
 include_once $_SERVER['DOCUMENT_ROOT']."/rankinginfo/word/PHPWord.php";
+header('Content-Type: application/vnd.ms-word');
+header('Content-Disposition: attachment;filename="Consulta.docx"');
+header('Cache-Control: max-age=0');
+
+$con = mysql_connect("localhost","root","crazyMarines910208");
+if (!$con)
+  {
+  die('Could not connect: ' . mysql_error());
+  }
+$database=mysql_select_db("rankinginfo") ;
+if (!$database)
+  {
+  die('Could not connect: ' . mysql_error());
+  }
+  
+  session_start();
+  if(!isset($_SESSION['nombreusuario'])){
+    echo '<script> window.location = "/rankinginfo/index.php"; </script>';
+	}
+	
+	
+	
+	
+
+/* Contador para llenar el arreglo de consultas */
+$i=0;
+$k=0;
+
+/* GET */
+$consultas = $_SESSION['consultadescarga'];
+/* Llenado del arreglo */
+foreach($consultas as $consulta)
+{
+   $curp[$i]= $consulta;
+   $sql[$i]="SELECT * from arrendado WHERE curp='".$curp[$i]."' ";
+   
+   
+   
+   $resultado[$i]=mysql_query($sql[$i],$con);
+   if (!$resultado[$i]) { // add this check.
+    die('Invalid query: ' . mysql_error());
+    }
+
+   $i++;
+}
 
 
+$j=0;
+foreach($consultas as $consulta)
+{
+   $curp[$j]= $consulta;
+   $sql1[$j]="SELECT * from arr_calif as a, calificacion as c WHERE a.curp = '".$curp[$j]."' GROUP BY c.clave";
+   
+   $resultado1[$j]=mysql_query($sql1[$j], $con); 
+   $j++;
+}
 
 // New Word Document
 $PHPWord = new PHPWord();
@@ -9,8 +63,19 @@ $PHPWord = new PHPWord();
 // New portrait section
 $section = $PHPWord->createSection();
 
+ while($row=mysql_fetch_array($resultado[$k], MYSQL_BOTH)) { 
+ 
+$section->addLink($nalgas, $nalgas, array('color'=>'0000FF', 'underline'=>PHPWord_Style_Font::UNDERLINE_SINGLE));
+
+}
+
+
+
+
+
+
+
 // Add hyperlink elements
-$section->addLink('http://www.google.com', 'Best search engine', array('color'=>'0000FF', 'underline'=>PHPWord_Style_Font::UNDERLINE_SINGLE));
 $section->addTextBreak(2);
 
 $PHPWord->addLinkStyle('myOwnLinkStyle', array('bold'=>true, 'color'=>'808000'));
@@ -19,25 +84,9 @@ $section->addLink('http://www.yahoo.com', null, 'myOwnLinkStyle');
 
 
 
-
-// Save File
-$objWriter = PHPWord_IOFactory::createWriter($PHPWord, 'Word2007');
-header('Content-Type: application/vnd.ms-word');
-header('Content-Disposition: attachment;filename="Consulta.docx"');
-header('Cache-Control: max-age=0');
-$objWriter->save("php://output");
-
-
-
-?>
-
-
-<!--
 // At least write the document to webspace:
 $objWriter = PHPWord_IOFactory::createWriter($PHPWord, 'Word2007');
 
-header('Content-Type: application/vnd.ms-word');
-header('Content-Disposition: attachment;filename="Consulta.docx"');
-header('Cache-Control: max-age=0');
+
 $objWriter->save("php://output");
--->
+?>
