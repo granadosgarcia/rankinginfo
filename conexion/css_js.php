f9e8f8986f526d3041130f008edf9c76971d3f45
Martin Granados <margrangr@gmail.com>
2012-12-10 13:39:11 -0600

    jquery

--- conexion/css_js.php
+++ conexion/css_js.php
@@ -3,3 +3,24 @@
 <script type='text/javascript' language='javascript' src='/rankinginfo/js/jquery-1.8.2.min.js'></script>
 
 <link rel="icon" href="/rankinginfo/img/favicon.ico" >
+
+<script type="text/javascript">
+		$(document).ready(function() {
+
+			$(".imagengrande").fancybox({
+				padding: 0,
+
+				openEffect : 'elastic',
+				openSpeed  : 150,
+
+				closeEffect : 'elastic',
+				closeSpeed  : 150,
+
+				closeClick : true,
+
+				helpers : {
+					overlay : null
+				}
+			});
+		});
+</script>
