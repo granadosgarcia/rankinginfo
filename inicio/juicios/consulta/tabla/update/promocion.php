c886997508f19119b485b4e9e7db832828bb5ca9
Martin Granados <margrangr@gmail.com>
2013-03-25 21:42:26 -0600

    promocion y updatepromo php

--- inicio/juicios/consulta/tabla/update/promocion.php
+++ inicio/juicios/consulta/tabla/update/promocion.php
@@ -3,22 +3,26 @@ include_once $_SERVER['DOCUMENT_ROOT']."/rankinginfo/conexion/sesion.php";
 include_once $_SERVER['DOCUMENT_ROOT']."/rankinginfo/conexion/con.php";
 
 ?>
-<<<<<<< HEAD
 
-
-
-<div id="primerrow">
-		<ul style="  list-style-type: none;">
-		
-		
-		<h1>Notificación</h1>
-			<li><label>Notificación</label><input type="text" name="actor_nombres" id="actor_nombres">	</li>
-=======
 <!DOCTYPE html>
 
 <html>
 <head>
+
 <meta charset="UTF-8"/>
+<link rel="stylesheet" href="/rankinginfo/css/estilo.css" type="text/css" charset="utf-8">
+
+  <link rel="stylesheet" href="http://code.jquery.com/ui/1.10.1/themes/base/jquery-ui.css" />
+  <script src="http://code.jquery.com/jquery-1.9.1.js"></script>
+  <script src="http://code.jquery.com/ui/1.10.1/jquery-ui.js"></script>
+
+  <script>
+  $(function() {
+    $( "#fecha_notificacion" ).datepicker({ dateFormat: "yy-mm-dd" });
+        $( "#fecha_promocion" ).datepicker({ dateFormat: "yy-mm-dd" });
+
+  });
+ </script>
 </head>
 <body>
 	<div >
@@ -26,10 +30,11 @@ include_once $_SERVER['DOCUMENT_ROOT']."/rankinginfo/conexion/con.php";
 	</div>
 	<div>
 		<form action="update_promocion.php" method="GET">
-		<ul>
-			<li><label>Comentario</label><input type="text" name="comentario" id="comentario"/></li>
-			<li><label>Fecha de Notificación</label><input type="date" name="fecha_notificacion" id="fecha_notificacion"/></li>
-			<li><label>Fecha de Promoción</label><input type="date" name="fecha_promocion" id="fecha_promocion"/></li>
+		<ul style="list-style: none;">
+			<li><label>Comentario</label><br>
+			<textarea rows="10" cols="35" id="comentario" name="comentario"></textarea></li>
+			<li><label>Fecha de Notificación</label><input type="text" name="fecha_notificacion" id="fecha_notificacion"/></li>
+			<li><label>Fecha de Promoción</label><input type="text" name="fecha_promocion" id="fecha_promocion"/></li>
 			<li><input type="submit"/></li>
 		</ul>
 
@@ -38,4 +43,3 @@ include_once $_SERVER['DOCUMENT_ROOT']."/rankinginfo/conexion/con.php";
 
 </body>
 </html>
->>>>>>> eb5c588d62a3fb49ad16b7eb72277124d5d77c98
--- inicio/juicios/consulta/tabla/update/update_promocion.php
+++ inicio/juicios/consulta/tabla/update/update_promocion.php
@@ -2,6 +2,8 @@
 include_once $_SERVER['DOCUMENT_ROOT']."/rankinginfo/conexion/sesion.php";
 include_once $_SERVER['DOCUMENT_ROOT']."/rankinginfo/conexion/con.php";
 
+
+
 $sqlact="
 INSERT INTO actividades (id_usuario, fecha, actividad)
 VALUES ('$_SESSION[usuario_id]', NOW(), 'Insertar Promocion')";
@@ -9,14 +11,23 @@ VALUES ('$_SESSION[usuario_id]', NOW(), 'Insertar Promocion')";
 $kevin = mysql_real_escape_string($_GET["comentario"]);
 $kevin1 = mysql_real_escape_string($_GET["fecha_notificacion"]);
 $kevin2 = mysql_real_escape_string($_GET["fecha_promocion"]);
+ $curp= mysql_real_escape_string($_SESSION['curp']);
+
+
+$originalDate = $kevin1;
+$kevin1 = date("Y-m-d", strtotime($originalDate));
+
+$originalDate1 = $kevin2;
+$kevin2 = date("Y-m-d", strtotime($originalDate1));
 
 $sql="INSERT INTO promocion(";
 if(!empty($_GET['comentario']))
-$sql.="promocion,";
+$sql.="comentario,";
 if(!empty($_GET['fecha_notificacion']))
 $sql.="fecha_notificacion,";
 if(!empty($_GET['fecha_promocion']))
 $sql.="fecha_promocion,";
+$sql.="expediente";
 $sql.=")";
 $sql.="VALUES(";
 if(!empty($_GET['comentario']))
@@ -24,7 +35,9 @@ $sql.="'$kevin',";
 if(!empty($_GET['fecha_notificacion']))
 $sql.="'$kevin1',";
 if(!empty($_GET['fecha_promocion']))
-$sql.="'$kevin2'";
+$sql.="'$kevin2',";
+$sql.="'$curp'";
+
 
 $sql.=")";
 
@@ -36,12 +49,11 @@ $sql.=")";
   }
 $id = mysql_insert_id();
  mysql_query($sqlact,$con);
-echo '<script> alert("Calificacion Agregada Exitosamente"); window.location = "../../"; </script>';
+echo '<script> alert("Calificacion Agregada Exitosamente"); window.close(); </script>';
 
  mysql_close($con);
- }
  
- else echo '<script> window.close(); </script>';
+ 
 ?>
 <!DOCTYPE html>
 
