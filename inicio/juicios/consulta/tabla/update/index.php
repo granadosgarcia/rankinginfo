c7ac158639ace0817a7411a8143da054f9891935
abrgnz <crazymarines2@gmail.com>
2013-03-26 17:02:59 -0600

    hola

--- inicio/juicios/consulta/tabla/update/index.php
+++ inicio/juicios/consulta/tabla/update/index.php
@@ -110,64 +110,50 @@ while($cont2>0){
 	
 	<label>Expediente</label>
 		<p><?php echo $row['expediente']?></p>
-			</br>	
 			
 			
 		<label>Actor</label>
 		<br>
 		<br><label>Nombres</label><p><?php echo $row['actor_nombres']?></p>
-	<br>
 		<label>Apellido Paterno</label>
 		<p><?php echo $row['actor_apellido_paterno']?> </p>
-			</br>
 
 		<label>Apellido Materno</label>
 		<p> <?php echo $row['actor_apellido_materno']?></p>
-			</br>
 			
 					<label>Demandado</label>
 		<br>
 		<br><label>Nombres</label><p><?php echo $row['demandado_nombres']?></p>
-	<br>
 		<label>Apellido Paterno</label>
 		<p><?php echo $row['demandado_apellido_paterno']?> </p>
-			</br>
 
 		<label>Apellido Materno</label>
 		<p><?php echo $row['demandado_apellido_materno']?></p>
-			</br>
 		<label>Juicio</label>
 		<p><?php echo $row['juicio']?></p>
-			</br>			
 			
 					
 			
 					<label>Juzgado</label>
 		<p><?php echo $row['juzgado']?></p>
-			</br>	
 			
 			
-														<label>Distrito Judicial</label>
-		<input type="text" name="distrito_juidicial"    id="distrito_juidicial"		value="<?php echo $row['distrito_juidicial']?>" class="inputderecha">
-			</br>	
+														<label>Distrito Judicial</label>		<input type="text" name="distrito_juidicial"    id="distrito_juidicial"		value="<?php echo $row['distrito_juidicial']?>" class="inputderecha">
 			
 					
 					<label>ultima_actuacion</label>
 		<input type="text" name="utlima_actuacion"    id="ultima_actuacion"		value="<?php echo $row['ultima_actuacion']?>" class="inputderecha">
-			</br>	
 			
 					<label>S.Actuación</label>
 		<input type="text" name="s_actuacion_01"    id="s_actuacion_01"		value="<?php echo $row['s_actuacion_01']?>" class="inputderecha">
-			</br>	
 			
 								<label>S.Actuación</label>
 		<input type="text" name="s_actuacion_02"    id="s_actuacion_02"		value="<?php echo $row['s_actuacion_02']?>" class="inputderecha">
-			</br>	<br>
+			</br>
 			
 			
 								<label>Estado Procesal</label><br>
 		<input type="text" name="estado_procesal"    id="estado_procesal"		value="<?php echo $row['estado_procesal']?>" class="inputderecha">
-			</br>	
 			
 	
 			
@@ -175,7 +161,7 @@ while($cont2>0){
 											
 		<input type="text" id="datepicker" name="datepicker" value="<?php echo $row['fecha']?>" />
 											
-			</br><br>
+			</br>
 			
 											<label>Comentario</label>
 		<textarea  name="comentario_01"    id="comentario_01"		 rows="6" cols="35"><?php echo $row['comentario_01']?></textarea>
