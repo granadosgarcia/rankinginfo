096cf357a82e5ec15c467c644ae00b7e78b6f73d
Martin Granados <margrangr@gmail.com>
2013-04-07 21:51:55 -0500

    EL FAMOSO ERROR HANDLING1

--- conexion/errorsql.php
+++ conexion/errorsql.php
@@ -1,5 +1,13 @@
 <?php
+//Si el error es 1062 es un error de que ya existe la clave (CURP,Expediente)
+
+//Avisa eso y regresa
 
 
-1062
+
+
+//Si el error no es esto hace una entrada en un log .txt con la fecha el sql 
+//y el errrno 
+
+//una explicación de que hubo un error en alert y regresa a la página anterior.
 ?>
